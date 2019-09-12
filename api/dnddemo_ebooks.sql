71b20b118d4b23ae4ba30dbf40c62faa5b5d4af2
AMIT00031 <54016595+AMIT00031@users.noreply.github.com>
2019-09-12 18:49:17 +0530

    Add files via upload

--- api/dnddemo_ebooks.sql
+++ api/dnddemo_ebooks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 10, 2019 at 02:03 PM
+-- Generation Time: Sep 12, 2019 at 01:15 PM
 -- Server version: 5.6.45-log
 -- PHP Version: 7.2.7
 
@@ -94,6 +94,32 @@ CREATE TABLE `setting` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_bookmark`
+--
+
+CREATE TABLE `tbl_bookmark` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(250) NOT NULL,
+  `books_id` varchar(250) NOT NULL,
+  `status` varchar(250) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_bookmark`
+--
+
+INSERT INTO `tbl_bookmark` (`id`, `user_id`, `books_id`, `status`, `created_at`, `updated_at`) VALUES
+(1, '6', '14', '1', '2019-09-12 09:54:08', '2019-09-12 10:14:49'),
+(2, '7', '14', '0', '2019-09-12 10:16:30', '2019-09-12 12:17:30'),
+(3, '7', '13', '0', '2019-09-12 10:17:14', '2019-09-12 12:35:57'),
+(4, '1', '14', '1', '2019-09-12 12:39:40', '2019-09-12 12:39:40'),
+(5, '1', '13', '1', '2019-09-12 12:40:14', '2019-09-12 01:02:12');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_books`
 --
 
@@ -110,7 +136,6 @@ CREATE TABLE `tbl_books` (
   `video_url` varchar(255) DEFAULT NULL,
   `audio_url` varchar(255) DEFAULT NULL,
   `pdf_url` varchar(255) DEFAULT NULL,
-  `bookMark` varchar(255) DEFAULT NULL,
   `mostView` varchar(255) DEFAULT '0',
   `status` int(11) DEFAULT '0',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -121,25 +146,21 @@ CREATE TABLE `tbl_books` (
 -- Dumping data for table `tbl_books`
 --
 
-INSERT INTO `tbl_books` (`id`, `user_id`, `category_id`, `book_title`, `book_slug`, `thubm_image`, `book_description`, `author_name`, `book_image`, `video_url`, `audio_url`, `pdf_url`, `bookMark`, `mostView`, `status`, `created_at`, `updated_at`) VALUES
-(1, '12', '1', 'Test book', 'test book', 'book_1567857479.jpg', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ', 'Raj Amit', NULL, NULL, NULL, '', NULL, '0', 1, '2019-09-07 00:00:00', '2019-09-07 11:57:59'),
-(2, '12', '1', 'Test book', 'test book', 'book_1567861435.jpg', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ', 'Raj Amit', 'gallery_1567861435.jpg', 'video_1567861435.mp4', 'audio_1567861435.mp3', 'document_1567861435.docx', NULL, '0', 1, '2019-09-07 00:00:00', '2019-09-07 13:03:55'),
-(3, '1', '1', 'video', 'video', 'book_1568007056.', '', '', 'gallery_1568007056.', 'video_1568007056.mp4', 'audio_1568007056.', 'document_1568007056.', NULL, '0', 1, '2019-09-08 00:00:00', '2019-09-09 05:30:56'),
-(4, '1', '0', 'video', 'video', 'book_1568008965.', '', '', 'gallery_1568008965.', 'video_1568008966.mp4', 'audio_1568008965.', 'document_1568008965.', NULL, '0', 1, '2019-09-08 00:00:00', '2019-09-09 06:02:46'),
-(5, '1', 'music', 'video', 'video', 'book_1568025083.', '', '', 'gallery_1568025083.png', 'video_1568025083.', 'audio_1568025083.', 'document_1568025083.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 10:31:23'),
-(6, '1', '1', 'book', 'book', 'book_1568025164.', '', '', 'gallery_1568025164.jpg', 'video_1568025164.', 'audio_1568025164.', 'document_1568025164.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 10:32:44'),
-(7, '1', '1', 'book', 'book', 'book_1568025500.', '', '', 'gallery_1568025500.jpg', 'video_1568025500.', 'audio_1568025500.', 'document_1568025500.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 10:38:20'),
-(8, '1', '1', 'book', 'book', 'book_1568025578.', '', '', 'gallery_1568025578.jpg', 'video_1568025578.', 'audio_1568025578.', 'document_1568025578.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 10:39:38'),
-(9, '1', '1', 'book', 'book', 'book_1568025635.', '', '', 'gallery_1568025635.jpg', 'video_1568025635.', 'audio_1568025635.', 'document_1568025635.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 10:40:35'),
-(10, '1', '1', 'book', 'book', 'book_1568026354.', '', '', 'gallery_1568026354.jpg', 'video_1568026354.', 'audio_1568026354.', 'document_1568026354.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 10:52:34'),
-(11, '1', '1', 'book', 'book', 'book_1568028268.', '', '', 'gallery_1568028268.jpg', 'video_1568028268.', 'audio_1568028268.', 'document_1568028268.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 11:24:28'),
-(12, '1', '1', 'book', 'book', 'book_1568030373.jpg', '', '', 'gallery_1568030373.', 'video_1568030373.', 'audio_1568030373.', 'document_1568030373.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 11:59:33'),
-(13, '1', '1', 'book', 'book', 'book_1568030398.jpg', '', '', 'gallery_1568030398.', 'video_1568030398.', 'audio_1568030398.', 'document_1568030398.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 11:59:58'),
-(14, '1', '1', 'book', 'book', 'book_1568034523.jpg', '', '', 'gallery_1568034523.', 'video_1568034523.', 'audio_1568034523.', 'document_1568034523.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 13:08:43'),
-(15, '1', '1', 'book', 'book', 'book_1568034696.jpg', '', '', 'gallery_1568034696.', 'video_1568034696.', 'audio_1568034696.', 'document_1568034696.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-09 13:11:36'),
-(16, '1', '2', 'music', 'music', 'book_1568094479.jpg', 'did. her voice wrw email erw. ', '', 'gallery_1568094479.', 'video_1568094479.', 'audio_1568094479.', 'document_1568094479.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-10 05:47:59'),
-(17, '1', '2', 'ETG', 'etg', 'book_1568094565.jpg', 'ETGQ ETWvga ert to. ', '', 'gallery_1568094565.', 'video_1568094565.', 'audio_1568094565.', 'document_1568094565.', NULL, '0', 1, '2019-09-09 00:00:00', '2019-09-10 05:49:25'),
-(18, '1', '2', 'ETG', 'etg', 'book_1568094639.jpg', 'ETGQ ETWvga ert to. ', '', 'gallery_1568094639.', 'video_1568094639.', 'audio_1568094639.', 'document_1568094639.', '0', '0', 1, '2019-09-09 00:00:00', '2019-09-10 12:27:29');
+INSERT INTO `tbl_books` (`id`, `user_id`, `category_id`, `book_title`, `book_slug`, `thubm_image`, `book_description`, `author_name`, `book_image`, `video_url`, `audio_url`, `pdf_url`, `mostView`, `status`, `created_at`, `updated_at`) VALUES
+(1, '1', '2', 'Test book ', 'test book ', '', 'Test book ', 'Amit Kumar', 'gallery_1568181187.jpg', '', '', '', '0', 1, '2019-09-10 00:00:00', '2019-09-11 05:53:07'),
+(2, '1', '1', 'videofdjfdgf jsdfw ', 'videofdjfdgf jsdfw ', '', '', '', 'gallery_1568181396.png', '', '', '', '0', 1, '2019-09-10 00:00:00', '2019-09-11 07:21:01'),
+(3, '1', '1', 'videofdjfdgf jsdfw ', 'videofdjfdgf jsdfw ', '', '', '', 'gallery_1568181717.png', '', '', '', '0', 0, '2019-09-10 00:00:00', '2019-09-11 10:38:29'),
+(4, '1', '2', 'videofdjfdgf jsdfw ', 'videofdjfdgf jsdfw ', '', '', '', 'gallery_1568181722.png', '', '', '', '0', 0, '2019-09-10 00:00:00', '2019-09-11 06:02:02'),
+(5, '1', '2', 'Test book  fdf', 'test book  fdf', '', 'Test book  fdf', 'Amit Kumar', 'gallery_1568182149.jpg', '', '', '', '0', 0, '2019-09-10 00:00:00', '2019-09-11 06:09:09'),
+(6, '1', '1', 'video', 'video', '', '', '', 'gallery_1568185152.png', '', '', '', '0', 0, '2019-09-10 00:00:00', '2019-09-11 06:59:12'),
+(7, '1', '1', 'video', 'video', 'book_1568185184.png', '', '', 'gallery_1568185184.png', '', '', '', '0', 0, '2019-09-10 00:00:00', '2019-09-11 06:59:44'),
+(8, '1', '1', 'music', 'music', 'book_1568186185.png', 'ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', 'Vansh', '', '', '', '', '0', 0, '2019-09-11 00:00:00', '2019-09-11 07:16:25'),
+(9, '1', '2', 'Demo', 'demo', 'book_1568186216.png', 'ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', 'Vansh', '', '', '', '', '0', 0, '2019-09-11 00:00:00', '2019-09-11 07:16:56'),
+(10, '6', '1', 'Demo', 'demo', 'book_1568186225.png', 'ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', 'Vansh', '', '', '', '', '0', 0, '2019-09-11 00:00:00', '2019-09-11 07:25:52'),
+(11, '1', '1', 'Video book', 'video book', 'book_1568187287.png', 'ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', 'Vansh', '', 'video_1568187287.mp4', '', '', '0', 0, '2019-09-11 00:00:00', '2019-09-11 08:53:38'),
+(12, '6', '1', 'docbook', 'docbook', 'book_1568188327.png', 'ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', 'Vansh', '', 'video_1568188327.mp4', '', 'document_1568188327.docx', '0', 0, '2019-09-11 00:00:00', '2019-09-11 07:52:07'),
+(13, '1', '1', 'Demo Full', 'demo full', 'book_1568196455.png', 'ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', 'Vansh', '', 'video_1568196455.mp4', 'audio_1568196455.mp3', 'document_1568196455.docx', '0', 0, '2019-09-11 00:00:00', '2019-09-11 10:07:35'),
+(14, '6', '1', 'Demopdf', 'demopdf', 'book_1568196915.png', 'ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.', 'Vansh', '', 'video_1568196915.mp4', 'audio_1568196915.mp3', 'document_1568196915.pdf', '0', 0, '2019-09-11 00:00:00', '2019-09-11 13:08:46');
 
 -- --------------------------------------------------------
 
@@ -168,6 +189,23 @@ INSERT INTO `tbl_category` (`id`, `category_name`, `slug_url`, `status`, `thum_i
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_review`
+--
+
+CREATE TABLE `tbl_review` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(250) DEFAULT NULL,
+  `books_id` varchar(250) DEFAULT NULL,
+  `comment` text,
+  `rating` varchar(250) DEFAULT NULL,
+  `status` varchar(250) DEFAULT '1',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user_login_table`
 --
 
@@ -186,7 +224,7 @@ CREATE TABLE `user_login_table` (
   `confirmation_key` varchar(1000) NOT NULL,
   `date_added` int(11) DEFAULT NULL,
   `date_edited` int(11) DEFAULT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `status` tinyint(1) NOT NULL DEFAULT '1',
   `message_status` varchar(255) NOT NULL DEFAULT '1',
   `publisher_type` varchar(255) NOT NULL DEFAULT '0' COMMENT '0-reader,1-writer,2-reader-writer,3-publisher',
   `device_token` text NOT NULL,
@@ -202,12 +240,13 @@ CREATE TABLE `user_login_table` (
 --
 
 INSERT INTO `user_login_table` (`id`, `register_id`, `full_name`, `user_name`, `url`, `email`, `gender`, `phone_no`, `about_me`, `country`, `password`, `confirmation_key`, `date_added`, `date_edited`, `status`, `message_status`, `publisher_type`, `device_token`, `device_type`, `address`, `thumb_image`, `createdAt`, `global_posting`) VALUES
-(1, '318527', '', 'VAnsh', 'pic_1568111814.jpg', 'chaudhary.vanshraj@gmail.com', '', '', '', '', 'IA==', '', NULL, 1568028117, 0, '1', 'Writer', '', '', '', '', '2019-09-09 11:21:57', '1'),
-(2, '847942', '', 'Vansh raj', 'pic_1568108462.', 'vansh1996raj@gmail.com', '', '', '', '', 'IA==', '', NULL, 1568108462, 0, '1', 'Writer', '', '', '', '', '2019-09-10 09:41:02', '1'),
-(3, '675018', '', 'dfd', 'pic_1568110414.', 'dfdf', 'male', '', '', 'df', 'ZmRm', '', NULL, 1568110414, 0, '1', 'Writer', '', '', '', '', '2019-09-10 10:13:34', '1'),
-(4, '925860', '', 'fgg', 'pic_1568110461.', 'g', 'male', '', '', 'gfgf', 'Z2Zn', '', NULL, 1568110461, 0, '1', 'Reader', '', '', '', '', '2019-09-10 10:14:21', '1'),
-(5, '103969', '', 'dgdgdggdfgdfdsfd', 'pic_1568110536.', 'sdsf', 'male', '', '', 'sf', 'c2ZkZg==', '', NULL, 1568110536, 0, '1', 'Reader', '', '', '', '', '2019-09-10 10:15:36', '1'),
-(6, '286303', '', 'errer', 'pic_1568110964.jpg', 'frsdfsdf', 'male', '', '', 'sdfd', 'c2Rmc2Rmc2Zz', '', NULL, 1568110911, 0, '1', 'Writer', '', '', '', '', '2019-09-10 10:21:51', '1');
+(1, '318527', '', 'VAnsh', 'pic_1568111814.jpg', 'chaudhary.vanshraj@gmail.com', '', '', 'vjj', 'India', 'IA==', '', NULL, 1568028117, 1, '1', 'Writer', '', '', '', '', '2019-09-09 11:21:57', '1'),
+(2, '847942', '', 'Vansh raj', 'pic_1568108462.', 'vansh1996raj@gmail.com', '', '', '', '', 'IA==', '', NULL, 1568108462, 1, '1', 'Writer', '', '', '', '', '2019-09-10 09:41:02', '1'),
+(3, '675018', '', 'dfd', 'pic_1568110414.', 'dfdf', 'male', '', '', 'df', 'ZmRm', '', NULL, 1568110414, 1, '1', 'Writer', '', '', '', '', '2019-09-10 10:13:34', '1'),
+(4, '925860', '', 'fgg', 'pic_1568110461.', 'g', 'male', '', '', 'gfgf', 'Z2Zn', '', NULL, 1568110461, 1, '1', 'Reader', '', '', '', '', '2019-09-10 10:14:21', '1'),
+(5, '103969', '', 'dgdgdggdfgdfdsfd', 'pic_1568110536.', 'sdsf', 'male', '', '', 'sf', 'c2ZkZg==', '', NULL, 1568110536, 1, '1', 'Reader', '', '', '', '', '2019-09-10 10:15:36', '1'),
+(6, '286303', '', 'errer', 'pic_1568110964.jpg', 'frsdfsdf', 'male', '', '', 'sdfd', 'c2Rmc2Rmc2Zz', '', NULL, 1568110911, 0, '1', 'Writer', '', '', '', '', '2019-09-10 10:21:51', '1'),
+(7, '313076', '', 'sdd', 'pic_1568288403.jpg', 'dddd', 'male', '', 'dddddrtrt. rrtr RT ', 'ddd', 'ZGRk', '', NULL, 1568282898, 1, '1', 'Writer', '', '', '', '', '2019-09-12 10:08:18', '1');
 
 --
 -- Indexes for dumped tables
@@ -233,6 +272,12 @@ ALTER TABLE `role`
   ADD UNIQUE KEY `name` (`role`);
 
 --
+-- Indexes for table `tbl_bookmark`
+--
+ALTER TABLE `tbl_bookmark`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_books`
 --
 ALTER TABLE `tbl_books`
@@ -242,6 +287,12 @@ ALTER TABLE `tbl_books`
 -- Indexes for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_review`
+--
+ALTER TABLE `tbl_review`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -275,10 +326,16 @@ ALTER TABLE `role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tbl_bookmark`
+--
+ALTER TABLE `tbl_bookmark`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `tbl_books`
 --
 ALTER TABLE `tbl_books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tbl_category`
@@ -287,10 +344,16 @@ ALTER TABLE `tbl_category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `tbl_review`
+--
+ALTER TABLE `tbl_review`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user_login_table`
 --
 ALTER TABLE `user_login_table`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
