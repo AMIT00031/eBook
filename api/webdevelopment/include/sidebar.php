8072839281be456e99834172ce768b6b22891751
AMIT00031 <54016595+AMIT00031@users.noreply.github.com>
2019-11-14 17:44:39 +0530

    Add files via upload

---
+++ api/webdevelopment/include/sidebar.php
@@ -0,0 +1,84 @@
+<!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="dashboard.php" class="brand-link">
+      <img src="asset/img/logo_white.png" alt="UEbook Logo" class="brand-image" style="opacity: .8">
+      <span class="brand-text font-weight-light">&nbsp</span>
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview">
+            <a href="dashboard.php" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p> Dashboard </p>
+            </a>
+          </li>
+		  
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-book"></i>
+              <p>Cms <i class="fas fa-angle-left right"></i></p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="javascript:void(0);" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i><p>Book Category</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i><p>Books</p>
+                </a>
+              </li>
+              
+             
+              <li class="nav-item">
+                <a href="" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i><p>Contacts</p>
+                </a>
+              </li>
+			  
+			  <li class="nav-item">
+                <a href="blank.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i><p>Blank</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+		  
+		  <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i><p>User<i class="fas fa-angle-left right"></i></p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="javascript:void(0);" class="nav-link"><p>User List</p>
+                </a>
+              </li>
+			  <li class="nav-item">
+                <a href="javascript:void(0);" class="nav-link"><p>User Profile</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="javascript:void(0);" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i><p>Settings</p>
+                </a>
+              </li>
+			  <li class="nav-item">
+                <a href="javascript:void(0);" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i><p>Logout</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
